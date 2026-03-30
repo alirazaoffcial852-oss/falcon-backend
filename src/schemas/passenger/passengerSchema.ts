c3ad2e4c7f653600b286d2d41e7f4a0121812c9d
JawadUllah18 <jawadullah@hashloops.com>
2026-03-30 10:37:07 +0500

    remove email validation

--- src/schemas/passenger/passengerSchema.ts
+++ src/schemas/passenger/passengerSchema.ts
@@ -1,11 +1,6 @@
 import Joi from "joi";
 
 export const createPassengerSchema = Joi.object({
-	email: Joi.string().email().trim().required().messages({
-		"any.required": "Email is required",
-		"string.email": "Email must be valid",
-		"string.empty": "Email is required",
-	}),
 	name: Joi.string().trim().required().messages({
 		"any.required": "Name is required",
 		"string.empty": "Name is required",
